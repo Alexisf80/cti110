cb8dbe71d82337c6689b7f4fbf60fa16cf471588
Alexisf80 <furmagea8069@student.faytechcc.edu>
2024-09-15 14:20:31 -0400

    Add files via upload

---
+++ P1LAB1_FurmageAlexis.py
@@ -0,0 +1,10 @@
+#Alexis Furmage
+#09-12-2024
+#P1LAB1
+#Learning how to create a python program using IDLE.
+
+
+
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+print(f"Welcome to the course, {first_name} {last_name}!")
