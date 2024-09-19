fba4b080cf2d3d73debbc59640bb8dc0be62cd20
Alexisf80 <furmagea8069@student.faytechcc.edu>
2024-09-19 02:06:00 -0400

    Add files via upload
    1st PHW submission

---
+++ P1HW1_FurmageAlexis.py
@@ -0,0 +1,13 @@
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+result = base ** exponent
+
+print(f"\n{base} raised to the power of {exponent} is {result} !!\n")
+
+starting_integer = int(input("Enter a starting integer: "))
+add_integer = int(input("Enter an integer to add: "))
+subtract_integer = int(input("Enter an integer to subtract: "))
+
+total = starting_integer + add_integer - subtract_integer
+
+print(f"\n{starting_integer} + {add_integer} - {subtract_integer} is equal to {total}")
